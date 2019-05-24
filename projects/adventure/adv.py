f3492c75e1bc7bb2cb58c26ef3e9aacbfd80a2a2
yannafaith <yannafaith97@gmail.com>
2019-05-24 09:35:12 -0700

    structure here, about to take out loop with error

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -24,10 +24,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 
-# roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}]}
-# roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5, 'w': 11}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}], 9: [(1, 4), {'n': 8, 's': 10}], 10: [(1, 3), {'n': 9, 'e': 11}], 11: [(2, 3), {'w': 10, 'e': 6}]}
 
-roomGraph={0: [(3, 5), {'n': 1}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}]}
+roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5, 'w': 11}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}], 9: [(1, 4), {'n': 8, 's': 10}], 10: [(1, 3), {'n': 9, 'e': 11}], 11: [(2, 3), {'w': 10, 'e': 6}]}
+
+
 
 world.loadGraph(roomGraph)
 
@@ -41,56 +41,106 @@ player = Player("Name", world.startingRoom)
 traversalPath = []
 traversalGraph = {}
 
-while len(traversalGraph) < 2:
-    currentRoom = player.currentRoom.id
+stack = Stack()
+world = World()
+roomGraph={0: [(3, 5), {'n': 1}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}]}
+world.loadGraph(roomGraph)
 
-    print(f"Current room: {currentRoom} - Explored {len(traversalGraph)} rooms, {len(traversalPath)} steps")
+world.printRooms()
+player = Player("Name", world.startingRoom)
 
-    if currentRoom not in traversalGraph:
+
+
+traversalPath = []
+traversalGraph = {}
+stack = Stack()
+
+dirs = ['n', 's', 'e', 'w']
+oppDirs = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
+
+print(len(roomGraph))
+while len(traversalGraph) < len(roomGraph):
+    prevRoom = player.currentRoom.id
+    print('\n starting loop OVER \n')
+            
+    if prevRoom not in traversalGraph:
         exits = {}
-        print(player.currentRoom.getExits())
-        print(player.currentRoom.id)
-        for e in player.currentRoom.getExits():
-            exits[e] = "?"
-        traversalGraph[currentRoom] = exits
-    exits = traversalGraph[currentRoom]
-    print(f"    Exits: {exits}")
+        for way in player.currentRoom.getExits():
+            exits[way] = "?"
+        traversalGraph[prevRoom] = exits
+    exits = traversalGraph[prevRoom]
+    print(prevRoom, traversalGraph, '\n \n')
+    
+    if 'n'
 
-    dirs = ['n', 's', 'e', 'w']
-    oppDirs = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
-
-    for way in dirs:
-        print(way)
-
-        if way in exits and exits[way] == '?':
+    for way in exits:
+        print('looking', {way})
+        if exits[way] == '?':
+            
             player.travel(way)
             traversalPath.append(way)
-            print(traversalPath)
             newRoom = player.currentRoom.id
-            print(newRoom)
             exits[way] = newRoom
-            print(exits)
+            
+            print('directions taken', traversalPath)
+            print('exits at room before are:', exits)
+            print('new room', newRoom, '\n')
+            
+            if newRoom in traversalGraph:
+                print('YUUUUUP', player.currentRoom.getExits())
+                player.travel(random.choice(player.currentRoom.getExits()))
+                for way in player.currentRoom.getExits():
+                    print(way)
 
+                #player.travel(random.choice(player.currentRoom.getExits()))
+                
+
+            
+
+            
             if newRoom not in traversalGraph:
-                print('nope')
                 newRoom_exits = {}
-                print(player.currentRoom.getExits())
-                for e in player.currentRoom.getExits():
-                    print(e)
-                    newRoom_exits[e] = "?"
-                    newRoom_exits[oppDirs[way]] = currentRoom
-                    traversalGraph[newRoom] = newRoom_exits
-            else: 
-                traversalGraph[newRoom][oppDirs[way]] = currentRoom
-            stack.push(oppDirs[way])
+                stack.push(newRoom)
+                print(newRoom, 'PREVWOO')
+                
+                for way in player.currentRoom.getExits():
+                    print('exits', player.currentRoom.getExits())
+                    if way == oppDirs[traversalPath[-1]]:
+                        newRoom_exits[way] = prevRoom
+                    else:
+                        newRoom_exits[way] = "?"
+                        traversalGraph[newRoom] = newRoom_exits
+                    print('newroom exits : ', newRoom_exits)
 
-        else:
-            backTrackDirection = stack.pop()
-            if backTrackDirection is None:
-                break
-            player.travel(backTrackDirection)
-            traversalPath.append(backTrackDirection)
+                else:
+                    print('END \n TRAVERSAL GRAPH:',traversalGraph, '\n')
+                    print(stack.pop())
+                    backTrackDirection = stack.pop()
+                    if backTrackDirection is None:
+                        break
+                    else:
+                        player.travel(backTrackDirection)
+                        traversalPath.append(backTrackDirection)
 
+#####
+#                                        #
+#      017       002       014           #
+#       |         |         |            #
+#       |         |         |            #
+#      016--015--001--012--013           #
+#                 |                      #
+#                 |                      #
+#      008--007--000--003--004           #
+#       |         |                      #
+#       |         |                      #
+#      009       005                     #
+#       |         |                      #
+#       |         |                      #
+#      010--011--006                     #
+#                                        #
+
+#####
+          
 # TRAVERSAL TEST
 visited_rooms = set()
 player.currentRoom = world.startingRoom
@@ -109,10 +159,10 @@ else:
 
 # UNCOMMENT TO WALK AROUND
 
-player.currentRoom.printRoomDescription(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    else:
-        print("I did not understand that command.")
+# player.currentRoom.printRoomDescription(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     else:
+#         print("I did not understand that command.")
